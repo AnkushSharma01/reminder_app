4e4ab7ca0cdb52c9c223cf1762142b03df0ecef9
_Ankush_sharma <107449662+AnkushSharma01@users.noreply.github.com>
2024-09-14 12:57:21 +0530

    Update App.js

--- reminder-app-frontend/src/App.js
+++ reminder-app-frontend/src/App.js
@@ -16,15 +16,15 @@ function App() {
   // original code
   
   useEffect(() => {
-    axios.get("http://localhost:5000/getAllReminder").then(res => setReminderList(res.data))
+    axios.get("https://reminder-app-back.onrender.com").then(res => setReminderList(res.data))
   },[]) // if this [] is empty means, It will load defined component when it is load first time bydefault.
 
   const addReminder = async () => {
     try {
       // post request contain mssg and tym
-      await axios.post("http://localhost:5000/addReminder", { reminderMsg, remindAt });
+      await axios.post("https://reminder-app-back.onrender.com", { reminderMsg, remindAt });
   
-      const updated = await axios.get("http://localhost:5000/getAllReminder");
+      const updated = await axios.get("https://reminder-app-back.onrender.com");
       setReminderList(updated.data);
   
 
@@ -38,7 +38,7 @@ function App() {
   
   
     const deleteReminder = (id) =>{
-    axios.post("http://localhost:5000/deleteReminder", {id})
+    axios.post("https://reminder-app-back.onrender.com", {id})
     .then(res => 
 
       // updata the list again
